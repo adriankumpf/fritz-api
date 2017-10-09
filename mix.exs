203a05b28ca8f1d99f765696deb6906ebc4f343f
Adrian Kumpf <adrian@peaksandpies.com>
2017-10-09 19:49:03 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FritzApi.Mixfile do
   def project do
     [
       app: :fritz_api,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
